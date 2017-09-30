d207b370506422855ee91d7df861a98f7d7d89de
johnmcraig <jmcraig32884@gmail.com>
2017-09-29 23:31:40 -0400

    Added posting functionality and refactored.

--- routes/newPost.js
+++ routes/newPost.js
@@ -11,7 +11,22 @@ router.get('/', function (req, res, next) {
 
 
 router.post('/', function (req, res, next) {
-    res.send("Done!" + req.body.id);
+    var newPost = {};
+    newPost.id = req.body.id;
+    newPost.title = req.body.title;
+    newPost.author = {};
+    newPost.author.firstName = req.body.firstName;
+    newPost.author.lastName = req.body.lastName;
+    newPost.author.email = req.body.email;
+    newPost.pubDate = req.body.pubDate;
+    newPost.content = req.body.content;
+
+    repo.addPost(newPost);
+
+    res.redirect('/');
+
+
+    // res.send("Done!" + req.body.id);
 });
 
 
